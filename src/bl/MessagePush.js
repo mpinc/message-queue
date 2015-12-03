c014ceac0d52d25d851588643e61e2e09661e0d7
xueling795 <ling.xue@missionpublic.com>
2015-12-03 16:42:53 +0800

    update queue sms process

--- src/bl/MessagePush.js
+++ src/bl/MessagePush.js
@@ -2,10 +2,12 @@
  * Created by ling xue on 15-10-29.
  */
 var xinge = require("xinge");
+var apn = require("apn");
 var xingeUtil = require("../util/XingeUtil.js");
 var sysConfig = require('../config/SystemConfig.js');
 var serverLogger = require('../util/ServerLogger.js');
 var logger = serverLogger.createLogger('MessagePush.js');
+var iosPushDao = require("../dao/IosPushDAO.js");
 
 var XingeApp = new xinge.XingeApp(sysConfig.xingeOptions.accessId, sysConfig.xingeOptions.secretKey);
 
@@ -18,6 +20,22 @@ function pushToSingoAndroidDevice(params,callback){
     })
 }
 
+
+
+function pushToSingleIosDevice(params,callback){
+    var message = new apn.notification();
+    var device = new apn.Device(params.deviceToken);
+    message.setAlertText(params.title);
+    message.badge = params.badge
+    message.sound = "ping.aiff";
+    message.payload = {'messageFrom': sysConfig.iosPushConfig.messageFrom};
+    message.expiry = Math.floor(Date.now() / 1000) + 3600;
+    iosPushDao.pushNotification({device:device,message:message},function(error,result){
+        callback(error,result);
+    });
+}
+
 module.exports ={
-    pushToSingoAndroidDevice : pushToSingoAndroidDevice
+    pushToSingoAndroidDevice : pushToSingoAndroidDevice ,
+    pushToSingleIosDevice :  pushToSingleIosDevice
 }
---
+++ src/dao/IosPushDAO.js
@@ -0,0 +1,55 @@
+/**
+ * Created by ling xue on 15-12-3.
+ */
+var apn = require("apn");
+var sysConfig = require('../config/SystemConfig.js');
+var serverLogger = require('../util/ServerLogger.js');
+var sysError = require('../util/SystemError.js');
+var logger = serverLogger.createLogger('IosPushDAO.js');
+
+function errorHappened(err, notification){
+    console.log(err);
+    console.log(notification);
+}
+var apnOptions = sysConfig.iosPushConfig;
+apnOptions.errorCallback =errorHappened ;
+var service = new apn.connection(apnOptions);
+
+service.on("connected", function() {
+    logger.debug("Connected APN");
+});
+
+service.on("transmitted", function(notification, device) {
+    logger.info("Notification transmitted to:" + device.token.toString("hex"));
+});
+
+service.on("transmissionError", function(errCode, notification, device) {
+    logger.error("Notification caused error: " + errCode + " for device ", device, notification);
+    if (errCode === 8) {
+        logger.log("A error code of 8 indicates that the device token is invalid. This could be for a number of reasons - are you using the correct environment? i.e. Production vs. Sandbox");
+    }
+});
+
+service.on("timeout", function (err) {
+    logger.error("Connection APN Timeout");
+
+});
+
+service.on("disconnected", function() {
+    logger.error("Disconnected from APNS");
+    //console.log('5')
+});
+
+service.on("socketError", function(error){
+    logger.error(error.message);
+
+});
+
+function pushNotification(params,callback) {
+    service.pushNotification(params.message, params.device);
+    callback(null,{success:true});
+}
+
+module.exports = {
+    pushNotification : pushNotification
+}
